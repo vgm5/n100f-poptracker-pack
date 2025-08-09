a49bc9a074c06dd296eddb11c9118e1bca097af2
vgm5 <110358490+vgm5@users.noreply.github.com>
2025-08-08 19:06:32 -0500

    Introduced Autotracking, reworked layout
    Fixed up the poptracker to at least be functional as an itemtracker.
    
    Full inventory should now autotrack fully

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -1,137 +1,313 @@
-ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
-ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
-ScriptHost:LoadScript("scripts/autotracking/flag_mapping.lua")
-ScriptHost:LoadScript("scripts/autotracking/ap_helper.lua")
+
+require("scripts/autotracking/item_mapping")
+require("scripts/autotracking/location_mapping")
+require("scripts/autotracking/hints_mapping")
 
 CUR_INDEX = -1
-PLAYER_ID = -1
-TEAM_NUMBER = 0
+--SLOT_DATA = nil
 
-EVENT_ID=""
-KEY_ID=""
+SLOT_DATA = {}
+
+local highlight_lvl= {
+    [0] = Highlight.Unspecified,
+    [10] = Highlight.NoPriority,
+    [20] = Highlight.Avoid,
+    [30] = Highlight.Priority,
+    [40] = Highlight.None,
+}
+
+function has_value (t, val)
+    for i, v in ipairs(t) do
+        if v == val then return 1 end
+    end
+    return 0
+end
+
+function dump_table(o, depth)
+    if depth == nil then
+        depth = 0
+    end
+    if type(o) == 'table' then
+        local tabs = ('\t'):rep(depth)
+        local tabs2 = ('\t'):rep(depth + 1)
+        local s = '{'
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. tabs2 .. '[' .. k .. '] = ' .. dump_table(v, depth + 1) .. ','
+        end
+        return s .. tabs .. '}'
+    else
+        return tostring(o)
+    end
+end
+
+function forceUpdate()
+    local update = Tracker:FindObjectForCode("update")
+    update.Active = not update.Active
+end
+
+function onClearHandler(slot_data)
+    local clear_timer = os.clock()
+    
+    ScriptHost:RemoveWatchForCode("StateChange")
+    -- Disable tracker updates.
+    Tracker.BulkUpdate = true
+    -- Use a protected call so that tracker updates always get enabled again, even if an error occurred.
+    local ok, err = pcall(onClear, slot_data)
+    -- Enable tracker updates again.
+    if ok then
+        -- Defer re-enabling tracker updates until the next frame, which doesn't happen until all received items/cleared
+        -- locations from AP have been processed.
+        local handlerName = "AP onClearHandler"
+        local function frameCallback()
+            ScriptHost:AddWatchForCode("StateChange", "*", StateChange)
+            ScriptHost:RemoveOnFrameHandler(handlerName)
+            Tracker.BulkUpdate = false
+            forceUpdate()
+            print(string.format("Time taken total: %.2f", os.clock() - clear_timer))
+        end
+        ScriptHost:AddOnFrameHandler(handlerName, frameCallback)
+    else
+        Tracker.BulkUpdate = false
+        print("Error: onClear failed:")
+        print(err)
+    end
+end
 
 function onClear(slot_data)
-	CUR_INDEX = -1
-	resetLocations()
-	resetItems()
+    --SLOT_DATA = slot_data
+    CUR_INDEX = -1
+    -- reset locations
+    for _, location_array in pairs(LOCATION_MAPPING) do
+        for _, location in pairs(location_array) do
+            if location then
+                local location_obj = Tracker:FindObjectForCode(location)
+                if location_obj then
+                    if location:sub(1, 1) == "@" then
+                        location_obj.AvailableChestCount = location_obj.ChestCount
+                    else
+                        location_obj.Active = false
+                    end
+                end
+            end
+        end
+    end
+    -- reset items
+    for _, item_pair in pairs(ITEM_MAPPING) do
+        for item_type, item_code in pairs(item_pair) do
+            local item_obj = Tracker:FindObjectForCode(item_code)
+            if item_obj then
+                if item_obj.Type == "toggle" then
+                    item_obj.Active = false
+                elseif item_obj.Type == "progressive" then
+                    item_obj.CurrentStage = 0
+                    item_obj.Active = false
+                elseif item_obj.Type == "consumable" then
+                    if item_obj.MinCount then
+                        item_obj.AcquiredCount = item_obj.MinCount
+                    else
+                        item_obj.AcquiredCount = 0
+                    end
+                elseif item_obj.Type == "progressive_toggle" then
+                    item_obj.CurrentStage = 0
+                    item_obj.Active = false
+                end
+            end
+        end
+    end
+    PLAYER_ID = Archipelago.PlayerNumber or -1
+    TEAM_NUMBER = Archipelago.TeamNumber or 0
+    SLOT_DATA = slot_data
+    -- if Tracker:FindObjectForCode("autofill_settings").Active == true then
+    --     autoFill(slot_data)
+    -- end
+    -- print(PLAYER_ID, TEAM_NUMBER)
+    if Archipelago.PlayerNumber > -1 then
 
-	if slot_data == nil then
-		print("its fucked")
-		return
-	end
-
-	PLAYER_ID = Archipelago.PlayerNumber or -1
-	TEAM_NUMBER = Archipelago.TeamNumber or 0
-	
-	--print(dump_table(slot_data))
-
-	for k,v in pairs(slot_data) do
-		if k == "remove_roadblocks" then
-			for r,c in pairs(ROADBLOCK_CODES) do
-				Tracker:FindObjectForCode(c).CurrentStage = has_value(slot_data['remove_roadblocks'],r)
-			end
-		elseif SLOT_CODES[k] then
-			Tracker:FindObjectForCode(SLOT_CODES[k].code).CurrentStage = SLOT_CODES[k].mapping[v]
-		end
-	end
-
-	if PLAYER_ID>-1 then
-		updateEvents(0)
-		EVENT_ID="pokemon_emerald_events_"..TEAM_NUMBER.."_"..PLAYER_ID
-		Archipelago:SetNotify({EVENT_ID})
-		Archipelago:Get({EVENT_ID})
-
-		KEY_ID="pokemon_emerald_keys_"..TEAM_NUMBER.."_"..PLAYER_ID
-		Archipelago:SetNotify({KEY_ID})
-		Archipelago:Get({KEY_ID})
-	end
+        HINTS_ID = "_read_hints_"..TEAM_NUMBER.."_"..PLAYER_ID
+        Archipelago:SetNotify({HINTS_ID})
+        Archipelago:Get({HINTS_ID})
+    end
 end
 
 function onItem(index, item_id, item_name, player_number)
-	if index <= CUR_INDEX then
-		return
-	end
-	CUR_INDEX = index;
-	local v = ITEM_MAPPING[item_id]
-	if not v or not v[1] then
-		--print(string.format("onItem: could not find item mapping for id %s", item_id))
-		return
-	end
-	local obj = Tracker:FindObjectForCode(v[1])
-	if obj then
-		obj.Active = true
-	else
-		print(string.format("onItem: could not find object for code %s", v[1]))
-	end
+    if index <= CUR_INDEX then
+        return
+    end
+    local is_local = player_number == Archipelago.PlayerNumber
+    CUR_INDEX = index;
+    local item = ITEM_MAPPING[item_id]
+    if not item or not item[1] then
+        --print(string.format("onItem: could not find item mapping for id %s", item_id))
+        return
+    end
+    for _, item_pair in pairs(item) do
+        item_code = item_pair[1]
+        item_type = item_pair[2]
+        local item_obj = Tracker:FindObjectForCode(item_code)
+        if item_obj then
+            if item_obj.Type == "toggle" then
+                -- print("toggle")
+                item_obj.Active = true
+            elseif item_obj.Type == "progressive" then
+                -- print("progressive")
+                item_obj.Active = true
+            elseif item_obj.Type == "consumable" then
+                -- print("consumable")
+                item_obj.AcquiredCount = item_obj.AcquiredCount + item_obj.Increment * (tonumber(item_pair[3]) or 1)
+            elseif item_obj.Type == "progressive_toggle" then
+                -- print("progressive_toggle")
+                if item_obj.Active then
+                    item_obj.CurrentStage = item_obj.CurrentStage + 1
+                else
+                    item_obj.Active = true
+                end
+            end
+        else
+            print(string.format("onItem: could not find object for code %s", item_code[1]))
+        end
+    end
 end
 
 --called when a location gets cleared
 function onLocation(location_id, location_name)
-	local v = LOCATION_MAPPING[location_id]
-	if not v or not v[1] then
-		--print(string.format("onLocation: could not find location mapping for id %s", location_id))
-		return
-	end
-	local obj = Tracker:FindObjectForCode(v[1])
-	if obj then
-		obj.AvailableChestCount = 0
-	else
-		print(string.format("onLocation: could not find object for code %s", v[1]))
-	end
+    local location_array = LOCATION_MAPPING[location_id]
+    if not location_array or not location_array[1] then
+        print(string.format("onLocation: could not find location mapping for id %s", location_id))
+        return
+    end
+
+    for _, location in pairs(location_array) do
+        local location_obj = Tracker:FindObjectForCode(location)
+        -- print(location, location_obj)
+        if location_obj then
+            if location:sub(1, 1) == "@" then
+                location_obj.AvailableChestCount = location_obj.AvailableChestCount - 1
+            else
+                location_obj.Active = true
+            end
+        else
+            print(string.format("onLocation: could not find location_object for code %s", location))
+        end
+    end
 end
 
+function onEvent(key, value, old_value)
+    updateEvents(value)
+end
+
+function onEventsLaunch(key, value)
+    updateEvents(value)
+end
+
+-- this Autofill function is meant as an example on how to do the reading from slotdata and mapping the values to 
+-- your own settings
+-- function autoFill()
+--     if SLOT_DATA == nil  then
+--         print("its fucked")
+--         return
+--     end
+--     -- print(dump_table(SLOT_DATA))
+
+--     mapToggle={[0]=0,[1]=1,[2]=1,[3]=1,[4]=1}
+--     mapToggleReverse={[0]=1,[1]=0,[2]=0,[3]=0,[4]=0}
+--     mapTripleReverse={[0]=2,[1]=1,[2]=0}
+
+--     slotCodes = {
+--         map_name = {code="", mapping=mapToggle...}
+--     }
+--     -- print(dump_table(SLOT_DATA))
+--     -- print(Tracker:FindObjectForCode("autofill_settings").Active)
+--     if Tracker:FindObjectForCode("autofill_settings").Active == true then
+--         for settings_name , settings_value in pairs(SLOT_DATA) do
+--             -- print(k, v)
+--             if slotCodes[settings_name] then
+--                 item = Tracker:FindObjectForCode(slotCodes[settings_name].code)
+--                 if item.Type == "toggle" then
+--                     item.Active = slotCodes[settings_name].mapping[settings_value]
+--                 else 
+--                     -- print(k,v,Tracker:FindObjectForCode(slotCodes[k].code).CurrentStage, slotCodes[k].mapping[v])
+--                     item.CurrentStage = slotCodes[settings_name].mapping[settings_value]
+--                 end
+--             end
+--         end
+--     end
+-- end
+
 function onNotify(key, value, old_value)
-	if key == EVENT_ID then
-		updateEvents(value)
-	elseif key == KEY_ID then
-		updateVanillaKeyItems(value)
-	end
+    print("onNotify", key, value, old_value)
+    if value ~= old_value and key == HINTS_ID then
+        for _, hint in ipairs(value) do
+            if hint.finding_player == Archipelago.PlayerNumber then
+                updateHints(hint.location, hint.status)
+            end
+        end
+    end
 end
 
 function onNotifyLaunch(key, value)
-	if key == EVENT_ID then
-		updateEvents(value)
-	elseif key == KEY_ID then
-		updateVanillaKeyItems(value)
-	end
+    print("onNotifyLaunch", key, value)
+    if key == HINTS_ID then
+        for _, hint in ipairs(value) do
+            -- print("hint", hint, hint.found)
+            -- print(dump_table(hint))
+            if hint.finding_player == Archipelago.PlayerNumber then
+                updateHints(hint.location, hint.status)
+            end
+        end
+    end
 end
 
-function updateEvents(value)
-	if value ~= nil then
-		local gyms = 0
-		for i, code in ipairs(FLAG_EVENT_CODES) do
-			local bit = value >> (i - 1) & 1
-			if i < 9 then
-				gyms = gyms + bit
-				if has("op_bdg_off") then --mark badge if unrandomized
-					Tracker:FindObjectForCode(FLAG_BADGE_CODES[i]).Active = bit
-				end
-			end
-			if code == "harbormail" then --special case for handling harbor mail, do not overwrite active value in case obtained as filler item
-				Tracker:FindObjectForCode(code).Active = Tracker:FindObjectForCode(code).Active or bit
-			elseif #code>0 then
-				Tracker:FindObjectForCode(code).Active = bit
-			end
-		end
-		Tracker:FindObjectForCode("gyms").CurrentStage = gyms
-	end
+function updateHints(locationID, status) -->
+    local location_table = LOCATION_MAPPING[locationID]
+    for _, location in ipairs(location_table) do
+        local obj = Tracker:FindObjectForCode(location)
+        if obj then
+            obj.Highlight = highlight_lvl[status]
+        else
+            print(string.format("No object found for code: %s", location))
+        end
+    end
+    -- local item_codes = HINTS_MAPPING[locationID]
+    -- 
+    -- for _, item_table in ipairs(item_codes, clear) do
+    --     for _, item_code in ipairs(item_table) do
+    --         local obj = Tracker:FindObjectForCode(item_code)
+    --         if obj then
+    --             if not clear then
+    --                 obj.Active = true
+    --             else
+    --                 obj.Active = false
+    --             end
+    --         else
+    --             print(string.format("No object found for code: %s", item_code))
+    --         end
+    --     end
+    -- end
 end
 
-function updateVanillaKeyItems(value) 
-	if value ~= nil then
-		for i, obj in ipairs(FLAG_ITEM_CODES) do
-			local bit = value >> (i - 1) & 1
-			if obj.codes and has(obj.option) then
-				for i, code in ipairs(obj.codes) do 
-					Tracker:FindObjectForCode(code).Active = bit
-				end
-			end
-		end
-	end
-end
 
-Archipelago:AddClearHandler("clear handler", onClear)
+-- ScriptHost:AddWatchForCode("settings autofill handler", "autofill_settings", autoFill)
+Archipelago:AddClearHandler("clear handler", onClearHandler)
 Archipelago:AddItemHandler("item handler", onItem)
 Archipelago:AddLocationHandler("location handler", onLocation)
+
 Archipelago:AddSetReplyHandler("notify handler", onNotify)
 Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
+
+
+
+--doc
+--hint layout
+-- {
+--     ["receiving_player"] = 1,
+--     ["class"] = Hint,
+--     ["finding_player"] = 1,
+--     ["location"] = 67361,
+--     ["found"] = false,
+--     ["item_flags"] = 2,
+--     ["entrance"] = ,
+--     ["item"] = 66062,
+-- } 
